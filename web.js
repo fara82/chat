42e56b6712a7a8c3dc2be1a785fe07922832664a
Farhana Basrawala (fara82) <fbasrawala@gmail.com>
2013-02-07 04:26:29 -0500

    change the script to use socket io

--- web.js
+++ web.js
@@ -1,9 +1,27 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express.createServer(express.logger()),
+	io = require('socket.io').listen(app);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+// app.get('/', function(request, response) {
+//   response.send('Hello World!');
+// });
+
+app.configure(function () { 
+  app.set("transports", ["xhr-polling"]); 
+  app.set("polling duration", 10); 
+});
+
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
 
 var port = process.env.PORT || 5000;
