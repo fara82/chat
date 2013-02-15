3d5268d66c8e7731fde2cc747ab495189616d3bd
Farhana Basrawala (fara82) <fbasrawala@gmail.com>
2013-02-14 20:46:11 -0500

    initial commit for the proj after scaffolding

---
+++ chat/chat_test.js
@@ -0,0 +1,3 @@
+steal('./models/message_test.js',
+	'/Users/fbasrawala/Sites/chat/chat/message/create/create_test.js',
+	'/Users/fbasrawala/Sites/chat/chat/message/list/list_test.js');
---
+++ chat/message/create/create.js
@@ -0,0 +1,26 @@
+steal('can', '/Users/fbasrawala/Sites/chat/chat/models/message.js', './init.ejs', 'jquery/dom/form_params',
+	function (can, Message, initEJS) {
+
+	/**
+	 * @class /Users/fbasrawala/Sites/chat/chat/message/create
+	 * @alias MessageCreate
+	 * @parent index
+	 * @inherits jQuery.Controller
+	 * Creates messages
+	 */
+	return can.Control(
+	/** @Prototype */
+	{
+		init: function () {
+			this.element.html(initEJS());
+		},
+		submit: function (el, ev) {
+			ev.preventDefault();
+			el.find('[type=submit]').val('Creating...')
+			new Message(el.formParams()).save(function() {
+				el.find('[type=submit]').val('Create');
+				el[0].reset()
+			});
+		}
+	});
+});
